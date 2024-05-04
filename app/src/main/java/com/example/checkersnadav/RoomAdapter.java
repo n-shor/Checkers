85f289b364782bde0a26e03ebf8fdc0a8b757974
n-shor <nbm1401@gmail.com>
2024-05-04 18:27:21 +0300

    Made the rooms page display the room owners' usernames instead of the rooms' ids

--- app/src/main/java/com/example/checkersnadav/RoomAdapter.java
+++ app/src/main/java/com/example/checkersnadav/RoomAdapter.java
@@ -2,11 +2,19 @@ package com.example.checkersnadav;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.List;
 
 public class RoomAdapter extends ArrayAdapter<Room>
@@ -26,11 +34,24 @@ public class RoomAdapter extends ArrayAdapter<Room>
             convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
         }
 
-        TextView tvRoom = (TextView) convertView.findViewById(android.R.id.text1);
+        TextView tvRoom = convertView.findViewById(android.R.id.text1);
 
         if (room != null)
         {
-            tvRoom.setText("Room Name: " + room.getRoomName() + ", Room ID: " + room.getRoomId());
+            DatabaseReference roomOwnerRef = FirebaseDatabase.getInstance().getReference("users").child(room.getRoomOwnerId());
+            roomOwnerRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    tvRoom.setText("Room Name: " + room.getRoomName() + "\nRoom Owner Username: " + dataSnapshot.child("username").getValue(String.class));
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.d("Database Error while trying to retrieve room owner username", databaseError.getMessage());
+                }
+            });
+
             if (room.isGameOngoing())
             {
                 tvRoom.setTextColor(Color.RED); // Ongoing game
