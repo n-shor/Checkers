a4e658a70c8a3d9179978d59e6905a91eceda2ce
n-shor <nbm1401@gmail.com>
2024-05-04 06:46:27 +0300

    Updated notification logic

--- app/src/main/java/com/example/checkersnadav/AlarmScheduler.java
+++ app/src/main/java/com/example/checkersnadav/AlarmScheduler.java
@@ -7,7 +7,6 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.TimeZone;
@@ -21,9 +20,9 @@ public class AlarmScheduler {
         // Setting alarm time for midnight
         AlarmManager alarmManager = (AlarmManager) activity.getSystemService(ALARM_SERVICE);
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Jerusalem"));
-        calendar.set(Calendar.HOUR_OF_DAY, 4);
-        calendar.set(Calendar.MINUTE, 47);
-        calendar.set(Calendar.SECOND, 40);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
 
         // Add a day in case the calendar is already behind the system at the time of creating the alarm
@@ -33,9 +32,8 @@ public class AlarmScheduler {
         {
             calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
-        Toast.makeText(context, String.valueOf(calendar.getTimeInMillis() - System.currentTimeMillis()), Toast.LENGTH_SHORT).show();
 
-        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent);
+        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
     }
 
 }
